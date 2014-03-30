d30071f3d81ad23404a0a7b197bacce82b7ebd21
dsorg <dsorg@hsr.ch>
2014-03-30 22:57:56 +0200

    CSS

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Forschungskompetenzen</title>
+
+<link rel="stylesheet" type="text/css" href="css/styles.css" media="all">
+<link rel="stylesheet" type="text/css" href="css/font.css" media="all">
+<link rel="stylesheet" type="text/css" href="css/menu.css" media="all">
+<link rel="stylesheet" type="text/css" href="css/event.css" media="all">
+<link rel="stylesheet" type="text/css" href="css/footer.css" media="all">
+
+</head>
+
+<body>
+
+<h1>Herzlich Willkommen zu Forschungskompetenzen</h1>
+
+<div id="main">
+	<div id="layout">
+    	<div id="header"></div>
+    </div>
+</div>
+
+</body>
+</html>
