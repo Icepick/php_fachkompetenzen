643bc6a8b805b35b620c327c22131610f7beb374
alufolie <yasemin.uensal@outlook.ch>
2014-04-01 11:00:53 +0200

    Verbindung und Auflistung.php
    In Ordner entpackt

---
+++ verbindung.php
@@ -0,0 +1,19 @@
+<?php
+// Wichtigste Verbindungsparameter als Variablen
+$host = "localhost"; 
+$db = "pokemon";
+$user = "root";
+$pwd = "";
+$tbl = "tbl_pokemon3";
+$tblf = "tbl_farbe";
+$verb = mysqli_connect($host, $user, $pwd, $db);
+
+// Fehler ausgeben
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+// Zeichensatz utf8
+mysqli_query($verb,"SET NAMES 'utf8' COLLATE 
+'utf8_general_ci'") or die("Fehler:
+".mysqli_connect_error());
+// Keine Enter unterhalb letzter Zeile!
+?>
