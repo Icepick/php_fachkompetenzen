dd54ad74ec2e35f0c9bb6a96b1b404065c598aac
alufolie <yasemin.uensal@outlook.ch>
2014-05-23 22:19:01 +0200

    revert

--- search.php
+++ search.php
@@ -2,6 +2,23 @@
 <?php
 //1) Verbindung zu MySQL öffnen
 $verb = openMySqlConnection();
+<<<<<<< HEAD
+<<<<<<< HEAD
+// <<<<<<< HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
+
+$eingabe = trim(@$_POST['eingabe']);
+
+///////////// SUCHRESULTATE ///////////////
+<<<<<<< HEAD
+<<<<<<< HEAD
+// =======
+=======
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
 ?>
 
 <?php
@@ -12,18 +29,40 @@ if(!empty($_POST['eingabe'])) {
 }
 
 
+<<<<<<< HEAD
+// >>>>>>> FETCH_HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> FETCH_HEAD
+>>>>>>> parent of 9cdae5c... << auskommentiert
 
-$checkboxStr = "";
 
-// Suchabfrage für Checkboxen /////
+////////////////// --1-- Nur Checkboxen, keine Eingabe ///////////////////////
 
-if(!empty($_POST['checkbox'])) {
+if(!empty($_POST['checkbox'])) { 
+	if(strlen($eingabe) == 0) {
 	echo "Gewählte Kategorien:";
-	$count = count($_POST['checkbox']);
-	$i = 0;
     foreach($_POST['checkbox'] as $check) {
             echo "<br />";
             echo $check;
+<<<<<<< HEAD
+<<<<<<< HEAD
+// <<<<<<< HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
+    }  
+?>
+    
+<table class="contenttable" id="resultattabelle">
+<<<<<<< HEAD
+<<<<<<< HEAD
+// =======
+=======
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
             $checkboxStr .= $check;
             
             $i++;
@@ -35,39 +74,42 @@ if(!empty($_POST['checkbox'])) {
 ?>
     
 <table width="" class="contenttable">
+<<<<<<< HEAD
+// >>>>>>> FETCH_HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> FETCH_HEAD
+>>>>>>> parent of 9cdae5c... << auskommentiert
 
 <tr>
-    <td rowspan="1"><strong>Name / Vorname</strong></td>
-    <td rowspan="1"><strong>Kategorie</strong></td>		 		
-    <td rowspan="1"><strong>Spezifikation</strong></td>
-    <td rowspan="1"><strong>Institut</strong></td>
-    <td rowspan="1"><strong>Kontakt</strong></td>
+    <td><strong>Name / Vorname</strong></td>
+    <td><strong>Kategorien</strong></td>		 		
+    <td><strong>Spezifikation</strong></td>
+    <td><strong>Institut</strong></td>
+    <td><strong>Kontakt</strong></td>
+</tr>
+<tr>
+    <td><strong>Nur Checkboxen, keine Eingabe</strong></td>
 </tr>
 <?php 
-			//2) SQL Abfrage ausführen
+	//2) SQL Abfrage ausführen
 		$result = executeSqlQuery($verb, sqlSearch2());
-		while ($resultat = mysqli_fetch_array($result)) { 
+		while ($resultat = mysqli_fetch_assoc($result)) { 
 ?>
 <tr>
 <td rowspan="1"> 
 		<?php echo $resultat['vorname']; ?>&nbsp<? echo $resultat['nachname']; ?>
-
 </td>
 <td rowspan="1"> 
 <?php 
-	 //2) SQL Abfrage ausführen
-	 $result2 = executeSqlQuery($verb, sqlSearch2());
-	 while ($kategorienausgabe = mysqli_fetch_array($result2)){ 
-	 echo $kategorienausgabe['kategorienname'];
-	 } ?>
+	 echo $resultat['kategorienname'];
+?>
 	 </td>
 <td rowspan="1"> 
 <?php 
-	 //2) SQL Abfrage ausführen
-	 $result3 = executeSqlQuery($verb, sqlSearch2());
-	 while ($spezifikationenausgabe = mysqli_fetch_array($result3)){ 
-	 echo $spezifikationenausgabe['spezifikationsname'];
-	 } ?>
+	 echo $resultat['spezifikationsname'];
+?>
 </td>
 <td rowspan="1"> 
 		<?php echo $resultat['institutsname']; ?>
@@ -77,30 +119,155 @@ if(!empty($_POST['checkbox'])) {
 </td>
 </tr>
       </table>  
-<?php } } ?>
+
+<?php } } }
+
+/////////////////////// --2-- Checkboxen leer, nur Eingabe ////////////////////////
+
+if(empty($_POST['checkbox'])) {
+	if(strlen($eingabe) > 0) { ?>
+    
+<table class="contenttable" id="resultattabelle">
+
+<tr>
+    <td><strong>Name / Vorname</strong></td>
+    <td><strong>Kategorien</strong></td>		 		
+    <td><strong>Spezifikation</strong></td>
+    <td><strong>Institut</strong></td>
+    <td><strong>Kontakt</strong></td>
+</tr>
+<tr>
+    <td><strong>Nur Eingabe, keine Checkboxen</strong></td>
+</tr>
+<?php 
+	//2) SQL Abfrage ausführen
+		$result = executeSqlQuery($verb, sqlSearch3());
+		while ($resultat = mysqli_fetch_array($result)) { 
+?>
+<tr>
+<td rowspan="1"> 
+		<?php echo $resultat['vorname']; ?>&nbsp<? echo $resultat['nachname']; ?>
+</td>
+<td rowspan="1"> 
+<?php 
+	 echo $resultat['kategorienname'];
+?>
+	 </td>
+<td rowspan="1"> 
+<?php 
+	 echo $resultat['spezifikationsname'];
+?>
+</td>
+<td rowspan="1"> 
+		<?php echo $resultat['institutsname']; ?>
+</td>
+<td rowspan="1"> 
+		<?php echo $resultat['mail']; ?>
+</td>
+</tr>
+      </table>  
+
+<?php }}}
+
+/////////////////////// --2-- Checkboxen UND Eingabe ////////////////////////
+
+if(!empty($_POST['checkbox'])) {
+	if(strlen($eingabe) > 0) { ?>
+    
+<table class="contenttable" id="resultattabelle">
+
+<tr>
+    <td><strong>Name / Vorname</strong></td>
+    <td><strong>Kategorien</strong></td>		 		
+    <td><strong>Spezifikation</strong></td>
+    <td><strong>Institut</strong></td>
+    <td><strong>Kontakt</strong></td>
+</tr>
+    <td><strong>Checkboxen UND Eingabe</strong></td>
+
+<?php 
+			//2) SQL Abfrage ausführen
+		$result = executeSqlQuery($verb, sqlSearch4());
+		while ($resultat = mysqli_fetch_array($result)) { 
+?>
+<tr>
+<td rowspan="1"> 
+		<?php echo $resultat['vorname']; ?>&nbsp<? echo $resultat['nachname']; ?>
+</td>
+<td rowspan="1"> 
+<?php 
+	 echo $resultat['kategorienname'];
+?>
+	 </td>
+<td rowspan="1"> 
+<?php 
+	 echo $resultat['spezifikationsname'];
+?>
+</td>
+<td rowspan="1"> 
+		<?php echo $resultat['institutsname']; ?>
+</td>
+<td rowspan="1"> 
+		<?php echo $resultat['mail']; ?>
+</td>
+</tr>
+      </table>  
+<?php
+ }} }?>
+
+<!--  //////////////// SUCHE //////////////////  -->
 
 <?php
-
-
-
-//////////////// SUCHE //////////////////
-
-if(strlen($checkboxStr) == 0) { ?> 
+if(empty($_POST['checkbox'])) {
+	if(empty($eingabe)) {
+ ?> 
 
 <div id="content_center_top"> 
   	                
 <h1>Fachkompetenzen suchen</h1>
-
+ 
+ <div id="content_center"> 
 
 <div id="komptable"> 
 
 <h2>Kategorie auswählen</h2>
 
-<p color:#d43f3a;".">Bitte wählen Sie eine Kategorie aus.</p><br />
-
 
 <form  name="form1" method="post" action="<?php echo "index.php?page=" . $page ?>"  id="searchform"> 
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+// <<<<<<< HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
+<table id="searchtable">
+
+<?php 
+	//2) SQL Abfrage ausführen
+	$result = executeSqlQuery($verb, sqlSearch1());
+?>
+ 
+<?php while ($search = mysqli_fetch_array($result)) { ?>
+
+<tr>
+<td>
+<label>
+	<input name="<?php echo "cB".$search['name']; ?>" class="checkbox" type="checkbox" value="<?php echo $search['name']; ?>" 
+ id="checkbox[<?php echo $search['name']; ?>]" > 
+	&nbsp;<?php echo $search['name']; ?>
+</label>
+</td>
+</tr>
+<?php } ?>             
+        </table> 
+<<<<<<< HEAD
+<<<<<<< HEAD
+// =======
+=======
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
 <div id="searchtable">
 	<?php 
 		//2) SQL Abfrage ausführen
@@ -129,13 +296,41 @@ if(strlen($checkboxStr) == 0) { ?>
   
     <input id="tags" class="search" type="text" name="eingabe">
     <input type="submit" name="submit" value="Suche" style="margin-top:10px;"> 
+<<<<<<< HEAD
+// >>>>>>> FETCH_HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> FETCH_HEAD
+>>>>>>> parent of 9cdae5c... << auskommentiert
 
 </div>
 
 <div id="suchfunktion">
-	<br/>
-	<h2>Suchergebnis</h2>
+
+  <h2>Suche verfeinern</h2>
   
+<<<<<<< HEAD
+<<<<<<< HEAD
+// <<<<<<< HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
+    <input  id="tags" type="text" name="eingabe" style="width:200px;">  <br/>
+    <input  type="submit" name="submit" value="Suche" style="margin-top:10px;"> 
+
+</div>
+
+</form>
+<br/>    
+<p color:#d43f3a;".">Bitte wählen Sie eine Kategorie aus oder geben Sie einen Suchbegriff ein.</p><br />
+<<<<<<< HEAD
+<<<<<<< HEAD
+// =======
+=======
+=======
+>>>>>>> parent of 9cdae5c... << auskommentiert
     <?php
 		if(isset($eingabe) && !empty($eingabe)) {
 			$result = executeSqlQuery($verb, ultimateTextSearch($eingabe));
@@ -160,15 +355,24 @@ if(strlen($checkboxStr) == 0) { ?>
 
 
 </div>
+<<<<<<< HEAD
+// >>>>>>> FETCH_HEAD
+=======
+>>>>>>> parent of ab32ee4... Merge branch 'master' of https://github.com/Icepick/php_fachkompetenzen
+=======
+>>>>>>> FETCH_HEAD
+>>>>>>> parent of 9cdae5c... << auskommentiert
 
+	</div>
+    </div>
 	
 <?php
-}
-	//4) Verbindung zu MySQL schliessen
+}  }
+//4) Verbindung zu MySQL schliessen
 	closeMySqlConnection($verb);
 ?>
 	
-<!-- Autokorrektur -->
+<!-- Autokorrektur
 
 <script src="js/jquery-2.0.3.min.js"></script>
 <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
@@ -178,28 +382,12 @@ if(strlen($checkboxStr) == 0) { ?>
 			"CSS",
 			"HTML",
 			"Chinesisch",
-			"BASIC",
-			"C",
-			"C++",
-			"Clojure",
-			"COBOL",
-			"ColdFusion",
-			"Erlang",
-			"Fortran",
-			"Groovy",
-			"Haskell",
-			"Java",
-			"JavaScript",
-			"Lisp",
-			"Perl",
-			"PHP",
-			"Python",
-			"Ruby",
-			"Scala",
+			"SQL",
+			"Eventplanung",
 			"Scheme" // beim letzten Wert kein Komma mehr!
 		];
 		$( "#tags" ).autocomplete({
 			source: availableTags
 		});
 	});
-	</script> 
+	</script>  -->
